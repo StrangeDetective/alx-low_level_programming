611e702840e4fbf9d987458121572798296be207
StrangeDetective <b.abdelrahman004@gmail.com>
2023-07-14 07:53:22 -0700

    T 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i <= 16 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
