2ba93a66968bc62fc7c34dbd422e99ffd0ee0921
StrangeDetective <b.abdelrahman004@gmail.com>
2023-07-14 07:16:55 -0700

    T 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,15 @@
  *
  * Return: 0 when successful
  */
+
 int main (void)
 {
 	char i;
 
-	for (i = "a" ; i <= "z" ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	for (i = "A" ; i <= "Z" ; i++)
+	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar(i);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
