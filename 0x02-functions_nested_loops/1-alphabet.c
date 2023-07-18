307b59f5fbfff805dc114526024b8001fd0fc08d
StrangeDetective <b.abdelrahman004@gmail.com>
2023-07-18 06:36:15 -0700

    T0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - main function
  *
- * Return:always 0
+ * Return: 0 when successful
  */
 
-void print_alphabet(void)
+int main(void);
 {
-	int i;
-
-	for (i = 97 ; i <= 122 ; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
